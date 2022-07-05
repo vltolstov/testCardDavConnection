319d99cb1ce8237b79c031134db808a743fa0817
vltolstov <tolstov@gmail.com>
2022-07-05 17:19:10 +0700

    Прототип получения списка E-mail c Synology NAS

--- index.php
+++ index.php
@@ -4,26 +4,31 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-//$ch = curl_init('195.208.146.48:8008/addressbooks/users/test/addressbook/');
+const USER = 'test'; //ввести актуальные данные
+const PASS = '*******'; //ввести актуальные данные
+
 $ch = curl_init('195.208.146.48:5000/carddav/Synology/f21b8d78-5741-4f11-965b-e62f4c21e328');
 
 curl_setopt($ch, CURLOPT_PORT, 5000);
-curl_setopt($ch, CURLOPT_USERPWD, 'test:QW!@we23');
+curl_setopt($ch, CURLOPT_USERPWD, USER . ':' . PASS);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
 curl_setopt($ch, CURLOPT_HEADER, false);
 
 $data = curl_exec($ch);
 
-$header_size		= curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-$http_code 			= curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-$pattern = "/[\._a-zA-Z0-9-]+@[\._a-zA-Z0-9-]+/";
-preg_match_all($pattern, $data, $result);
+if($http_code == 200 && $data) {
+    $pattern = "/[\._a-zA-Z0-9-]+@[\._a-zA-Z0-9-]+/";
+    preg_match_all($pattern, $data, $result);
+    $result = array_unique($result[0]);
 
-//$result = explode(':', $html);
-
-var_dump($data, $result, $header_size, $http_code);
+    var_dump($data, $result, $http_code);
+} else {
+    echo 'Данные или соединение отсутствуют';
+}
 
 curl_close($ch);
+
+
